ce315aabb53e8f58de655bedb4b2fab3842ece6d
Raj Patel <jgpatel7714@gmail.com>
2020-06-13 23:03:29 +0530

    hdms 1.0

---
+++ hostel/src/hostel/send_feedback.java
@@ -0,0 +1,75 @@
+package hostel;
+
+import java.util.Properties;    
+import javax.mail.*;    
+import javax.mail.internet.*;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class send_feedback
+ */
+@WebServlet("/send_feedback")
+public class send_feedback extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public send_feedback() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name=request.getParameter("name");
+		String email=request.getParameter("email");
+		String comment=request.getParameter("comments");
+		String subject="Feedback from "+name+" Email id="+email;
+		send("enr7714@gmail.com","enr@1234","jgpatel7714@gmail.com",subject,comment);
+		response.sendRedirect("demo.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	public void send(String from,String password,String to,String sub,String msg){  
+        //Get properties object    
+        Properties props = new Properties();    
+        props.put("mail.smtp.host", "smtp.gmail.com");    
+        props.put("mail.smtp.socketFactory.port", "465");    
+        props.put("mail.smtp.socketFactory.class",    
+                  "javax.net.ssl.SSLSocketFactory");    
+        props.put("mail.smtp.auth", "true");    
+        props.put("mail.smtp.port", "465");    
+        //get Session   
+        Session session = Session.getDefaultInstance(props,    
+         new javax.mail.Authenticator() {    
+         protected PasswordAuthentication getPasswordAuthentication() {    
+         return new PasswordAuthentication(from,password);  
+         }    
+        });    
+        //compose message    
+        try {    
+         MimeMessage message = new MimeMessage(session);    
+         message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));    
+         message.setSubject(sub);    
+         message.setText(msg);    
+         //send message  
+         Transport.send(message);    
+         System.out.println("message sent successfully");    
+        } catch (MessagingException e) {throw new RuntimeException(e);}    
+           
+  }  
+
+}
